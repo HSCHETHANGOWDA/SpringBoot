75ba8f50f67767b706640aff5c418b7bafc9e678
chethan <chetangowda@gmail.com>
2023-11-23 20:43:56 +0530

    My First Commit

---
+++ gowda/src/main/java/com/chethan/gowda/GowdaApplication.java
@@ -0,0 +1,13 @@
+package com.chethan.gowda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GowdaApplication.class, args);
+	}
+
+}
---
+++ gowda/src/test/java/com/chethan/gowda/GowdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chethan.gowda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
